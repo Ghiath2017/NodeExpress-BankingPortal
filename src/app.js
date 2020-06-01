c2b90d88adef3b58071e9fd48a2eb33cb48d37eb
Ghiath2017 <g.alshamaa@hotmail.com>
2020-06-01 14:40:55 +0200

    add some Explanations

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// bind the view with the Server
 app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'ejs');
 
+// bind the Public (CSS, Js) with the Server
 app.use(express.static(path.join(__dirname, '/public/')));
 
+// first Route '/'
 app.get('/', (req, res) => res.render('index', {title: 'Index'}));
 
+// bind the Server with the Port
 app.listen(3000, () => console.log('PS Project Running on Port 3000!'));
