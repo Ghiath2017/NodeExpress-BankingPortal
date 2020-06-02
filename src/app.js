dd4bf9f1339932d51898376c863a1b8ee2e7e822
Ghiath2017 <g.alshamaa@hotmail.com>
2020-06-02 13:45:10 +0200

    URL Encoded Middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const bodyParser = require("body-parser");
 
 const express = require('express');
 const app = express();
@@ -10,6 +11,9 @@ app.set('view engine', 'ejs');
 
 // bind the Public (CSS, Js) with the Server
 app.use(express.static(path.join(__dirname, '/public/')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const accountData = fs.readFileSync('src/json/accounts.json', {encoding:'utf8', flag:'r'});
 const accounts = JSON.parse(accountData);
